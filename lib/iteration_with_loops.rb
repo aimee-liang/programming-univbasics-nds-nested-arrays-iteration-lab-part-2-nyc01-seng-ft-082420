bbb269bf6db5c700cccbd7e2cb6d0b78c07a33d6
Aimee Liang <aimeeeliang@gmail.com>
2020-08-15 19:38:32 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -4,7 +4,7 @@ def find_min_in_nested_arrays(src)
 
   row_index = 0 
   while row_index < src.count do
-    element_index = 0 
+    element_index = 100 
     while element_index < src[row_index].count do
       temperature = src[row_index][0]
       if temperature < src[row_index][element_index]
